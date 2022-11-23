b86e449052db2dd0ad864a912c5141dc35a2bba9
Dylouwu <diloulefilou@gmail.com>
2022-11-23 14:20:32 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,23 @@
 //bonjour
+<<<<<<< HEAD
 //aurevoir
-void main(){
-    printf("hello");
+=======
+int main(int argc, char const *argv[]){
+    FILE *fic = NULL;
+    char choice = 0;
+    char fname[200]="";
+    while(choice != 'c' && choice != 'd'){
+        printf("Do you wish the compress (c) or decompress (d) a file ?");
+        scanf("%c", &choice);
+    }
+    if (choice == 'c'){
+        while(fic == NULL){ //check whether or not the file exists.
+                printf("Enter the path of the file you wish to compress");
+                scanf("%200s",fname);
+                fic=fopen(fname,"r");
+        }
+    }
+    fclose(fic);
+    return 0;
 }
+>>>>>>> e9e8beb0d28b1a2ad18bd49f5a67f67d812393d1
